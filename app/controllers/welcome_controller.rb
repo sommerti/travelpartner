5dda9e0146c5e9522c57054fd7a5794d3740fbe5
Owen Xia <owenyx@gmail.com>
2015-05-25 01:03:53 +0800

    wiki modification for heroku deployment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,10 +8,6 @@ class WelcomeController < ApplicationController
 
   def wiki
   	@country = Country.find(params[:country_id])
-    begin
-  	 @response = HTTParty.get("http://wikitravel.org/wiki/en/api.php?action=query&prop=extracts&format=json&titles=#{@country.country_name}")
-    rescue Exception
-     @response = nil
-    end
+  	@response = HTTParty.get("http://wikitravel.org/wiki/en/api.php?action=query&prop=extracts&format=json&titles=#{@country.country_name}")
   end
 end
