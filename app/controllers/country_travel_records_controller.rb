651ed2dcb59b6dd5c6fdb70bfbdb23f61c5ca4d3
Owen Xia <owenyx@gmail.com>
2015-05-24 11:45:28 +0800

    remove button

--- app/controllers/country_travel_records_controller.rb
+++ app/controllers/country_travel_records_controller.rb
@@ -1,25 +1,24 @@
 class CountryTravelRecordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_country
-  before_action :set_user
-  before_action :set_travel_status
+  before_action :set_country, except: [:destroy]
+  before_action :set_travel_status, except: [:destroy]
 
   def create_update
     # user might want to update a record or try to add the same country again 
     # so start by searching for an existing country travel record matching the user and the country
-    country_travel_record_id = @user.find_country_travel_record(@country)
+    country_travel_record_id = current_user.find_country_travel_record(@country)
 
     # if such a record does not yet exist, create new record
     if country_travel_record_id.nil?
 
       @country_travel_record = CountryTravelRecord.new
-      @country_travel_record.user_id = @user.id
+      @country_travel_record.user_id = current_user.id
       @country_travel_record.country_id = @country.id
       @country_travel_record.travel_status = @travel_status
 
       if @country_travel_record.save 
-        flash[:notice] = "You've marked #{@country.country_name} as 'wanna visit'." if @travel_status == "wannavisit"
-        flash[:notice] = "You've marked #{@country.country_name} as 'have been'." if @travel_status == "havebeen"
+        flash[:notice] = "You've marked '#{@country.country_name}' as 'wanna visit'." if @travel_status == "wannavisit"
+        flash[:notice] = "You've marked '#{@country.country_name}' as 'have been'." if @travel_status == "havebeen"
       else
         flash[:alert] = "Country not added."
       end
@@ -31,19 +30,25 @@ class CountryTravelRecordsController < ApplicationController
       @country_travel_record.travel_status = @travel_status
 
       if @country_travel_record.save 
-        flash[:notice] = "You've updated #{@country.country_name} as 'wanna visit'." if @travel_status == "wannavisit"
-        flash[:notice] = "You've updated #{@country.country_name} as 'have been'." if @travel_status == "havebeen"
+        flash[:notice] = "You've updated '#{@country.country_name}' as 'wanna visit'." if @travel_status == "wannavisit"
+        flash[:notice] = "You've updated '#{@country.country_name}' as 'have been'." if @travel_status == "havebeen"
       else
         flash[:alert] = "Country not added."
       end
 
     end
 
-    redirect_to travel_profile_user_path(@user)
+    redirect_to travel_profile_user_path(current_user)
 
   end
 
   def destroy
+    @country_travel_record = CountryTravelRecord.find(params[:id])
+    @country_name = @country_travel_record.country.country_name
+    @country_travel_record.destroy
+    flash[:notice] = "'#{@country_name}' has been removed."
+
+    redirect_to travel_profile_user_path(current_user)
   end
 
   private
@@ -54,10 +59,6 @@ class CountryTravelRecordsController < ApplicationController
 
   def set_country
     @country = Country.find(params[:country_id])
-  end
-
-  def set_user   
-    @user = User.friendly.find(params[:id])
   end
 
   def set_travel_status
